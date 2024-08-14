9bdd3b88dd1f0ccf1adc4e8a581b613e94c22814
GormlessGeorge <kulaev-george@yandex.ru>
2024-08-14 05:57:49 +0300

    job page created

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,5 @@
     <x-slot:heading>
         Home Page
     </x-slot:heading>
-    <h1>{{ $greeting }} from Home page</h1>
+
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,31 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home', ['jobs' => [
-        ['title' => 'Director', 'salary' => '$10.000'],
-        ['title' => 'Programmer', 'salary' => '$5.000'],
-        ['title' => 'Teacher', 'salary' => '$3.000']
-    ]]);
+    return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    return view('jobs', ['jobs' => [
+        ['id'=> 1,'title' => 'Director', 'salary' => '$10.000'],
+        ['id'=> 2,'title' => 'Programmer', 'salary' => '$5.000'],
+        ['id'=> 3,'title' => 'Teacher', 'salary' => '$3.000']
+    ]]);
 });
 
 Route::get('/contact', function () {
     return view('contact');
+});
+
+Route::get('/jobs/{id}', function ($id) {
+    $jobs = [
+        ['id'=> 1,'title' => 'Director', 'salary' => '$10.000'],
+        ['id'=> 2,'title' => 'Programmer', 'salary' => '$5.000'],
+        ['id'=> 3,'title' => 'Teacher', 'salary' => '$3.000']
+    ];
+    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+
+    return view('job', ['job'=> $job]);
 });
